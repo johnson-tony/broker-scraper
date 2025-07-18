f1a88587901011d3eff239d665aba248e38425a3
johnson-tony <johnsonsingam111@gmail.com>
2025-07-18 11:53:55 +0530

    Switched to full Puppeteer and added launch flags for Render

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: true, // Set false to see the browser
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
-  const page = await browser.newPage();
 
+  const page = await browser.newPage();
   await page.goto('https://example.com');
   const title = await page.title();
-
   console.log('Page title:', title);
 
   await browser.close();
